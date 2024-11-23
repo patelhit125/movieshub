e0627a23c75a9ade79306d54ef9c04f08254531c
patelhit125 <patelhit125@gmail.com>
2024-11-23 21:42:17 +0530

    fix: embed links

--- src/components/OverLay.js
+++ src/components/OverLay.js
@@ -1,44 +1,71 @@
-import React, { useRef } from 'react'
-import { FiX } from 'react-icons/fi';
-import { IoPlay } from 'react-icons/io5';
-import { EMBEDLINK } from '../constant';
+import React, { useRef } from "react";
+import { FiX } from "react-icons/fi";
+import { IoPlay } from "react-icons/io5";
+import { EMBEDLINK } from "../constant";
 
 const OverLay = (props) => {
-
   const { title, name, id, season, episode, play } = props;
   const overlayRef = useRef(null);
 
   const handleClick = () => {
     if (overlayRef.current.classList.contains("hide")) {
-      overlayRef.current.classList.remove("hide")
-      document.getElementById("body").classList.add("overflow")
-      document.getElementById('iframe').src = EMBEDLINK + name + '?id=' + id + '&s=' + season + '&e=' + episode;
-      document.getElementById('iframe').src = document.getElementById('iframe').src;
+      overlayRef.current.classList.remove("hide");
+      document.getElementById("body").classList.add("overflow");
+      document.getElementById("iframe").src = `${EMBEDLINK}${
+        name === "movie"
+          ? `embed/${id}`
+          : name === "tv"
+          ? `embedtv/${id}&s=${season}&e=${episode}`
+          : `https://2anime.xyz/embed/${title}${episode}`
+      }`;
+      document.getElementById("iframe").src =
+        document.getElementById("iframe").src;
     }
-  }
+  };
 
   const handleClose = () => {
     if (!overlayRef.current.classList.contains("hide")) {
-      overlayRef.current.classList.add("hide")
-      document.getElementById("body").classList.remove("overflow")
-      document.getElementById('iframe').src = '';
-      document.getElementById('iframe').src = document.getElementById('iframe').src;
+      overlayRef.current.classList.add("hide");
+      document.getElementById("body").classList.remove("overflow");
+      document.getElementById("iframe").src = "";
+      document.getElementById("iframe").src =
+        document.getElementById("iframe").src;
     }
-  }
+  };
 
   return (
-    <div className='btn-overlay'>
-      {title &&
+    <div className="btn-overlay">
+      {title && (
         <div>
-          <div className='btn-play' onClick={handleClick}><IoPlay /> {play}</div>
-          <div className='overlay hide' ref={overlayRef}>
-            <div className='close' onClick={handleClose}><FiX /></div>
-            <iframe id="iframe" className='w-100 h-100' title={title} frameBorder="0" allowFullScreen={true} width="100%" height="100%" loading="lazy" src={EMBEDLINK + name + '?id=' + id + '&s=' + season + '&e=' + episode}></iframe>
+          <div className="btn-play" onClick={handleClick}>
+            <IoPlay /> {play}
+          </div>
+          <div className="overlay hide" ref={overlayRef}>
+            <div className="close" onClick={handleClose}>
+              <FiX />
+            </div>
+            <iframe
+              id="iframe"
+              className="w-100 h-100"
+              title={title}
+              frameBorder="0"
+              allowFullScreen={true}
+              width="100%"
+              height="100%"
+              loading="lazy"
+              src={`${EMBEDLINK}${
+                name === "movie"
+                  ? `embed/${id}`
+                  : name === "tv"
+                  ? `embedtv/${id}&s=${season}&e=${episode}`
+                  : `https://2anime.xyz/embed/${title}${episode}`
+              }`}
+            ></iframe>
           </div>
         </div>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default OverLay
+export default OverLay;
